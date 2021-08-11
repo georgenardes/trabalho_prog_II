22818c7817eee18ad4a201dc7d7ce2ec740d740d
Luiz_Zimmermann <luizzimmermann@edu.univali.br>
2021-08-11 16:28:52 -0300

    Cadastro, deletar, objeto2linha

--- locadora/main.c
+++ locadora/main.c
@@ -14,17 +14,24 @@ typedef struct {
 void print_vector(Filme vetor[], int tam);
 void print_arq(FILE *arquivo);
 void escrita(char filme[255], FILE *arquivo);
-void cadastrar(FILE *arquivo);
+void cadastrar(Filme filmes[], int *tam);
+void deletar(Filme filmes[], int pos, int *tam);
 Filme* linha_para_filme (char *linha);
 void transfere(FILE *arquivo, Filme dados[], int *tam, char nome[50]);
 FILE *init_arquivo(char nome[50]);
+void objeto_para_arquivo(FILE *arquivo, Filme filmes[], int tam);
 
-
+//Função que recebe uma linha do arquivo de texto e transforma em uma variável do tipo Filme para armazenar as informações do filme
+//Recebe uma string que contem uma linha que estava no arquivo
 Filme* linha_para_filme (char *linha) {
+
+
+    // Aloca memória para a variável filme
     Filme *filme = malloc(sizeof(Filme));
 
     char *st = linha;
 
+    //Caractere que separa as informações
     const char delimiter[] = ";";
 
 
@@ -60,45 +67,103 @@ Filme* linha_para_filme (char *linha) {
         ch = strtok(NULL, delimiter);
         sprintf(filme->diretor, "%s", ch);
     }
-
+    /*
     printf("fim\n");
     printf("%s\n", filme->nome);
     printf("%s\n", filme->genero);
     printf("%d\n", (int)filme->ano);
     printf("%.2f\n", filme->duracao);
-    printf("%s\n", filme->diretor);
+    printf("%s\n", filme->diretor);*/
 
     return filme;
 }
+//Função que recebe o array de obejtos do tipo Filme e escreve cada filme no arquivo de texto
+//Recebe um ponteiro do tipo FILE, o array de Filmes e o tamanho do array
+void objeto_para_arquivo(FILE *arquivo, Filme filmes[], int tam){
 
-void cadastrar(FILE *arquivo){
+    //Abre o arquivo ou cria se necessário
+    arquivo = init_arquivo("dados.txt");
 
-    char filme[255] ;
+    for(int i=0; i<tam; i++){
+        char aux[255];
+        //Para cada filme no array de filmes, cria uma string que contem as informações do filme
+        sprintf(aux, "%s;%s;%d;%.2f;%s", filmes[i].nome, filmes[i].genero, (int)filmes[i].ano, filmes[i].duracao, filmes[i].diretor);
+        //Escreve a string no arquivo
+        escrita(aux, arquivo);
+    }
+    //Fecha o arquivo
+    fclose(arquivo);
+}
 
-    printf("Digite um filme: ");
-    scanf("%s", &filme);
+//Função que insere um filme no array de filmes
+//Recebe o array de filmes e o tamanho do array
+void cadastrar(Filme filmes[], int *tam){
 
-    printf("%s\n", filme);
-    escrita(filme, arquivo);
+    char aux[255];
+    if(*tam<255){
+
+
+        Filme *filme = malloc(sizeof(Filme));
+        printf("Digite o nome do filme: ");
+        scanf("%s", &filme->nome);
+        printf("Digite o genero do filme: ");
+        scanf("%s", &filme->genero);
+        printf("Digite o ano do filme: ");
+        scanf("%hu", &filme->ano);
+        printf("Digite a duracao do filme: ");
+        scanf("%f", &filme->duracao);
+        printf("Digite o diretor do filme: ");
+        scanf("%s", &aux);
+        sprintf(filme->diretor, "%s\n", aux);
+
+        filmes[*tam] = *filme;
+        (*tam)++;
+
+        printf("%d de %d filmes cadastrados\n", *tam, 255);
+
+    }else{
+        printf("Limite de filmes cadastrados atingido: %d de %d filmes cadastrados\n", *tam, 255);
+    }
 
     return;
 }
+//Função que remove do array de filmes
+//Recebe o array de Filmes, a posição que deseja ser deletada e tamanho do array
+void deletar(Filme filmes[], int pos, int *tam){
 
+    //Se por acaso receber uma posição maior que o tamanho total de filmes
+    if(!pos>(*tam)+1){
+        printf("Posicao maior que o tamanho do vetor");
+        return;
+    }
+    //Desloca todos os elemento apos a posição "para a esquerda" do array
+    for(int i = pos - 1; i < (*tam) - 1; i++){
+        filmes[i] = filmes[i+1];
+    }
+
+    (*tam)--;
+
+    return;
+
+}
+//Função que Recebe uma string que contem as informações do filme e que será escrita em um arquivo
+//Recebe uma string com as informações e um ponteiro do tipo FILE
 void escrita(char filme[255], FILE *arquivo){
 
         int er;
+        //Escreve a string no arquivo
         er = fputs(filme, arquivo);
 
         if(er == EOF){
             printf("Erro ao escrever uma linha do arquivo.");
         }else{
-
-            printf("Filme registrado com sucesso!!");
+            printf("Filme registrado com sucesso!!\n");
         }
 
 }
-
+//Auxiliar
 void print_arq(FILE *arquivo){
+
 
     printf("Conteudo do arquivo: \n");
     char saux[255];
@@ -106,7 +171,7 @@ void print_arq(FILE *arquivo){
         printf("%s\n", saux);
     }
 }
-
+//Auxiliar
 void print_vector(Filme vetor[], int tam){
 
     printf("Filmes\n");
@@ -121,68 +186,79 @@ void print_vector(Filme vetor[], int tam){
 
     return;
 }
-
+//Função que copia os dados de um arquivo txt e passa para o array de Filmes para serem manipulados durante a execução do programa
+//Recebe um ponteiro do tipo FILE, o array do tipo Filme, o tamanho do array e o nome do arquivo que irá ser aberto
 void transfere(FILE *arquivo, Filme dados[], int *tam, char nome[50]){
 
+
     printf("Transferencia de dados %d\n", *tam);
+
+    //Abre ou criar o arquivo
+    arquivo = init_arquivo("dados.txt");
 
     char saux[255];
     int iaux = 0;
     int er;
-
+    //Enquanto não chegar ao fim do arquivo continua lendo linha por linha
     while(fgets(saux, 255, arquivo) != NULL){
-        printf("%s\n", saux);
-        dados[iaux] = *linha_para_filme(saux);
+        printf("%d %s\n", *tam, saux);
+        //Converte a linha para uma variavel e insere no array
+        dados[*tam] = *linha_para_filme(saux);
         (*tam)++;
     }
     printf("Terminou de ler o arquivo. O arquivo contem %d linhas\n", *tam);
-    //fclose(arquivo);
-    //remove(nome);
+    //Fecha o arquivo
+    fclose(arquivo);
+    //Exclui o arquivo
+    remove(nome);
 
     return;
 }
-
+//Função que abre o arquivo para leitura e escrita, se o arquivo não existir, ela irá criar
+//Recebe uma string com o nome do arquivo
 FILE *init_arquivo(char nome[50]){
 
     FILE *arquivo;
-
+    //Abre um arquivo txt
     arquivo = fopen(nome, "r+");
 
     if(arquivo == NULL){
         printf("Nao ha um arquivo %s para salvar. Criando novo arquivo.\n", nome);
+        //Cria um arquivo txt se não houver
         arquivo = fopen(nome, "w+");
         if(arquivo == NULL){
             printf("Nao foi possivel criar o arquivo. Encerrando programa!!!\n");
             return NULL;
         }
-
     }
-
     return arquivo;
+}
+
+void busca(int op){
+
+
 
 }
 
 int main()
 {
-
+    //Variável que irá receber o arquivo
     FILE *arquivo;
-    FILE *aux_arquivo;
-
-    arquivo = init_arquivo("dados.txt");
-    aux_arquivo = init_arquivo("auxiliar.txt");
 
     int op = 0;
+    //Array do tipo Filme que irá armazenar os filmes durante a execução do código
     Filme dados[255];
-
-    //menu();
-
+    //Tamanho do array dados
     int tam = 0;
-    //transfere(arquivo, aux_arquivo, "dados.txt");
+
+
     transfere(arquivo, dados, &tam, "dados.txt");
+    //cadastrar(dados, &tam);
+    deletar(dados, 3, &tam);
 
-    printf("Nome do filme: %s\n", dados[0].nome);
+    print_vector(dados, tam);
 
-    //print_vector(dados, tam);
+    objeto_para_arquivo(arquivo, dados, tam);
     //print_arq(arquivo);
     //print_arq(aux_arquivo);
 
@@ -193,21 +269,13 @@ int main()
 
     case 0:
         printf("Cadastrar\n");
-        cadastrar(aux_arquivo);
+        //cadastrar(aux_arquivo);
 
         break;
     case 1:
         printf("Opcao 2\n");
         break;
     }
-
-
-
-    char linha[] = "Nome;Genero;2000;2;Nolan";
-    Filme *filme = linha_para_filme(linha);
-    printf("linha %s\n", linha);
-
-    // menu();
 
     printf("Fim do programa!!\n");
     getch();
