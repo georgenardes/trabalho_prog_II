886f050b9e53c7aeb72e6f6280c52ed38dc86b9d
Luiz_Zimmermann <luizzimmermann@edu.univali.br>
2021-09-07 16:53:49 -0300

    Todas as funções implementadas, falta interface

--- tarefa2/lista.h
+++ tarefa2/lista.h
@@ -4,35 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Contato{
-
-    char nome[30];
-    char email[50];
-    char telefone[15];
-
-};
-
-struct Elemento
+typedef struct Econtato
 {
-    struct Contato contato;
+
+    char *nome;
+    char *email;
+    char *telefone;
+
+} Contato;
+
+typedef struct LElemento
+{
+    Contato contato;
     //Aponta pro proximo elemento da lista
-    struct Elemento *proximo;
+    struct LElemento *proximo;
 
-};
+} Elemento;
 
-struct lista_e
+typedef struct listaE
 {
-    struct Elemento *inicio;
+    Elemento *inicio;
     int cont;
 
-};
+} Lista_e;
+
+Lista_e* criaLista();
+Elemento *novoElemento(char *nome, char *email, char *telefone);
+void insereContato(Lista_e *lista, char *nome, char *email, char *telefone);
+void removeContatoIndex (Lista_e *lista, int index);
+void removeContatoNome (Lista_e *lista, char *nome);
+void mostraContatos(Lista_e lista);
+void consultaContato(Lista_e lista, char *nome);
+int quantidadeContatos (Lista_e lista);
 
 
-struct Elemento *novoElemento (struct Contato contato);
-void insereContato(struct lista_e *lista, struct Contato elemento);
-void removeContato(struct lista_e *lista, int index);
-void mostraContatos(struct lista_e lista);
-int quantidadeContatos (struct lista_e lista);
-struct lista_e criaLista ();
+
 
 #endif // LISTA_H_INCLUDED
