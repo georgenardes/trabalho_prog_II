546d2ccd9765cd9b83ae01b1457927a24a75f1a9
georgenardes <georgenardes@gmail.com>
2021-09-10 15:35:05 -0300

    implementado interface para o programa arvores

--- tarefa3/arvore.c
+++ tarefa3/arvore.c
@@ -1,10 +1,22 @@
 #include "arvore.h"
 
-
+/**
+    Atribui Null ao elemento raiz da arvore
+*/
 void inicia (Arvere_t *arv){
     arv->raiz = NULL;
 }
 
+/**
+    Função recebe o nó raiz, o dado e a chave.
+
+    Caso o nó seja nulo, é criado um novo dado com alocação
+    de memória.
+
+    Se o nó não for nulo, é verificado se o dado a ser
+    inserido é menor ou maior, chamando a função insere
+    recursivamente com o novo nó raiz.
+*/
 void insere(Tno_t **no, TIPO dado, int chave){
      if ((*no) == NULL){
          (*no) = (Tno_t*) malloc(sizeof(Tno_t));
@@ -22,6 +34,13 @@ void insere(Tno_t **no, TIPO dado, int chave){
      }
 }
 
+/**
+
+    Printa preordem com a seguinte lógica recursiva:
+        Printa elemento
+        visita todos da esquerda.
+        visita todos da direita.
+*/
 void printa_preordem (Tno_t **no){
     if((*no) != NULL){
         printf("%d -> %d \n", (*no)->chave,(*no)->dado);
@@ -30,6 +49,12 @@ void printa_preordem (Tno_t **no){
     }
 }
 
+/**
+    Printa ordem com a seguinte lógica recursiva
+        visita todos da esquerda.
+        Printa elemento
+        visita todos da direita.
+*/
 void printa_ordem (Tno_t **no){
     if((*no) != NULL){
         printa_ordem(&((*no)->esquerda));
@@ -38,6 +63,12 @@ void printa_ordem (Tno_t **no){
     }
 }
 
+/**
+    Printa pos-ordem com a seguinte lógica recursiva
+        visita todos da esquerda.
+        visita todos da direita.
+        Printa elemento
+*/
 void printa_posordem (Tno_t **no){
     if((*no) != NULL){
         printa_posordem(&((*no)->esquerda));
@@ -46,18 +77,24 @@ void printa_posordem (Tno_t **no){
     }
 }
 
+/**
+    Remove nó
+
+    Seleciona o maior nó à esquerda ou o primeiro nó à direita
+    para substituir o nó a ser removido
+*/
 void remover(Tno_t **no){
     Tno_t *apagar;
     Tno_t *maior = (*no)->esquerda;
 
-    if(maior == NULL)
-    {
+    if(maior == NULL){
         apagar = (*no);
         (*no) = (*no)->direita;
         free(apagar);
         return;
     }
     Tno_t *pai = NULL;
+
     while(maior->direita != NULL){
         pai = maior;
         maior = maior->direita;
@@ -74,24 +111,31 @@ void remover(Tno_t **no){
     free(apagar);
 }
 
-void procura_remove(Tno_t **no, int chave){
+/**
+    Procura valor a ser removido
+*/
+void procura_remove(Tno_t **no, TIPO dado){
     if((*no) != NULL){
-        if((*no)->chave == chave)
+        if((*no)->dado == dado)
             remover(no);
-        else if((*no)->chave > chave)
-            procura_remove(&((*no)->esquerda), chave);
+        else if((*no)->dado > dado)
+            procura_remove(&((*no)->esquerda), dado);
         else
-            procura_remove(&((*no)->direita), chave);
+            procura_remove(&((*no)->direita), dado);
     }
 }
 
+/**
+    Libera memória alocada
+*/
 void libera_arvore(Arvere_t *arv){
     Tno_t ** raiz = &(arv->raiz);
 
     while((*raiz) != NULL){
-        printf("apagando %d\n", (*raiz)->dado);
-        procura_remove(raiz, (*raiz)->chave);
+        printf("%d\n", (*raiz)->dado);
+        procura_remove(raiz, (*raiz)->dado);
     }
-
+    free(raiz);
+    free(arv);
 }
 
--- tarefa3/main.c
+++ tarefa3/main.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
 #include "arvore.h"
+#include <windows.h>
+#include <conio.h>
 
 int main()
 {
     TIPO vet[8] = {33, 15, 41, 38, 34, 47, 43, 49};
     Arvere_t *arv = (Arvere_t *) malloc (sizeof(Arvere_t));
-
     inicia(arv);
-    Tno_t ** raiz = &(arv->raiz);
 
+    Tno_t ** raiz = &(arv->raiz);
     for (int i = 0; i < 8; i++){
         insere(raiz, vet[i], i);
     }
-    printf("printa_preordem\n");
-    printa_preordem(raiz);
-    printf("\n---\n");
-    printf("printa_ordem\n");
-    printa_ordem(raiz);
-    printf("\n---\n");
-    printf("printa_posordem\n");
-    printa_posordem(raiz);
-    printf("\n---\n");
 
+    int input_char = 0;
+    do {
+        system("cls");
+
+        printf("Digite o numero da opcao desejada ou ESC para encerrar o programa.\n\n");
+        printf("(1) Visita em pre-ordem\n");
+        printf("(2) Visita em ordem\n");
+        printf("(3) Vista em pos-ordem\n");
+
+        do {
+            input_char = _getch();
+        } while (input_char != 49 && input_char != 50 && input_char != 51 && input_char != 27);
+
+        system("cls");
+
+        if (input_char == 49){
+            printf("Selecionado opcao de vista em pre-ordem\n\n");
+            printa_preordem(raiz);
+            printf("\n---\n");
+            _getch();
+        } else if (input_char == 50){
+            printf("Selecionado opcao de vista em ordem\n\n");
+            printa_ordem(raiz);
+            printf("\n---\n");
+            _getch();
+        } else if (input_char == 51) {
+            printf("Selecionado opcao de vista em pos-ordem\n\n");
+            printa_posordem(raiz);
+            printf("\n---\n");
+            _getch();
+        } else if (input_char == 27) {
+            break;
+        }
+
+    } while (1);
+
+    printf("Liberando memoria...\n");
+
+    // libera memória
     libera_arvore(arv);
-
     return 0;
 }
 
